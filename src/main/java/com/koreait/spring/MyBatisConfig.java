815996df803769d2ed81b4f18a8a8d1886d12c03
Jiyeong-github <thisisjiyeong@gmail.com>
2021-06-02 17:37:37 +0900

    0602 : JOIN&LOGIN

---
+++ src/main/java/com/koreait/spring/MyBatisConfig.java
@@ -0,0 +1,34 @@
+package com.koreait.spring;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+// @Mapper interface 스캔 경로
+@MapperScan(basePackages = "com.koreait.spring")
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setTypeAliasesPackage("com.koreait.spring");
+        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+
+        return  sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) throws Exception{
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
